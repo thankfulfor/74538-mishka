f1119afe8bcc7798c328e7fd4971cf20ba7c446b
Жанна Нарсия <z.narsija@superjob.ru>
2016-12-28 18:45:46 +0300

    Удаляем папку build

--- build/js/modal.js
+++
@@ -1,9 +0,0 @@
-var popup = document.querySelector('.modal');
-var button = document.querySelector('.js-order-button');
-
-button.preventDefault;
-button.addEventListener('click', showPopup);
-
-function showPopup() {
-  popup.classList.remove('modal--hidden');
-}
